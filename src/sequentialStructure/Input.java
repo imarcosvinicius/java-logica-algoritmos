84b56d94d64d4f83c6dfe311d928cda7b1988131
Marcos Vinícius <imarcosvinicius@outlook.com>
2023-01-31 15:05:32 -0300

    input do usuario

---
+++ src/sequentialStructure/Input.java
@@ -0,0 +1,20 @@
+package sequentialStructure;
+
+import java.util.Scanner;
+
+public class Input {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x;
+        System.out.print("Informe o valor: ");
+        x = sc.nextInt();
+
+        System.out.println("\nVocê digitou: " + x);
+
+        sc.close();
+    }
+
+}
