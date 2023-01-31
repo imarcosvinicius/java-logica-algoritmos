13f32d3ff21c7461fe040c351be8baa7007f5bdb
Marcos Vinícius <imarcosvinicius@outlook.com>
2023-01-31 15:05:15 -0300

    exercicio 01

---
+++ src/sequentialStructure/Exer01.java
@@ -0,0 +1,24 @@
+package sequentialStructure;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exer01 {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double largura = sc.nextDouble();
+        double comprimento = sc.nextDouble();
+        double valor = sc.nextDouble();
+
+        double area = largura * comprimento;
+        double preco = area * valor;
+        System.out.printf("AREA = %.2f%n", area);
+        System.out.printf("PRECO = %.2f%n", preco);
+
+        sc.close();
+    }
+
+}
