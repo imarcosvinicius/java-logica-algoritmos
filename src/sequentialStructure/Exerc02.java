9338ae6a01a631e0862d7a41127ad04937d2add6
Marcos Vinícius <imarcosvinicius@outlook.com>
2023-01-31 18:39:21 -0300

    exerc 02 java

---
+++ src/sequentialStructure/Exerc02.java
@@ -0,0 +1,21 @@
+package sequentialStructure;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exerc02 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        double base, altura, area, perimetro, diagonalCalc, diagonal;
+        base = sc.nextDouble();
+        altura = sc.nextDouble();
+
+        area = base * altura;
+        perimetro = 2.0 * (base + altura);
+        diagonal = Math.sqrt(base * base + altura * altura);
+        System.out.printf("AREA = %.4f%n", area);
+        System.out.printf("PERIMETRO = %.4f%n", perimetro);
+        System.out.printf("DIAGONAL = %.4f%n", diagonal);
+    }
+}
